5005efd51e3fe83683afc64e8cad23a0a1f80e88
tadashimasuda <tmasu4260@gmail.com>
2021-09-25 15:46:01 +0900

    ITP1_1_D

--- aoj/lesson1.py
+++ aoj/lesson1.py
@@ -1,8 +1,14 @@
 #ITP1_1_B
-# X = int(input())
-# print(X ** 3)
+X = int(input())
+print(X ** 3)
 
 #ITP1_1_C
 input = input()
 a,b = [int(num) for num in input.split(' ')]
 print(str(a * b) + ' ' + str(a * 2 + b * 2))
+
+#ITP1_1_D
+input = int(input())
+h, mod = divmod(input, 3600)
+m,s = divmod(mod, 60)
+print(f"{h}:{m}:{s}")
