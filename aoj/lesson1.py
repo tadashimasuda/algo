46f6e460635cb0c558534e442a4338e797ed906d
tadashimasuda <tmasu4260@gmail.com>
2021-09-26 11:46:58 +0900

    ITP1_3_C

--- aoj/lesson1.py
+++ aoj/lesson1.py
@@ -1,57 +1,67 @@
-# #ITP1_1_B
-# X = int(input())
-# print(X ** 3)
-#
-# #ITP1_1_C
+#ITP1_1_B
+X = int(input())
+print(X ** 3)
+
+#ITP1_1_C
+input = input()
+a,b = [int(num) for num in input.split(' ')]
+print(str(a * b) + ' ' + str(a * 2 + b * 2))
+
+#ITP1_1_D
+input = int(input())
+h, mod = divmod(input, 3600)
+m,s = divmod(mod, 60)
+print(f"{h}:{m}:{s}")
+
+#ITP1_2_A
+input = input()
+a,b = [int(num) for num in input.split(' ')]
+if a<b:
+    print('a < b')
+elif a>b:
+    print('a > b')
+elif a == b:
+    print('a == b')
+
+# ITP1_2_B
+input = input()
+a,b,c = [int(num) for num in input.split(' ')]
+if a < b  and b < c:
+    print('Yes')
+else:
+    print('No')
+
+# ITP1_2_C
+input = input()
+list = [int(num) for num in input.split(' ')]
+list.sort()
+print(f"{list[0]} {list[1]} {list[2]}")
+
+#ITP1_2_D
 # input = input()
-# a,b = [int(num) for num in input.split(' ')]
-# print(str(a * b) + ' ' + str(a * 2 + b * 2))
-#
-# #ITP1_1_D
-# input = int(input())
-# h, mod = divmod(input, 3600)
-# m,s = divmod(mod, 60)
-# print(f"{h}:{m}:{s}")
-#
-# #ITP1_2_A
-# input = input()
-# a,b = [int(num) for num in input.split(' ')]
-# if a<b:
-#     print('a < b')
-# elif a>b:
-#     print('a > b')
-# elif a == b:
-#     print('a == b')
-#
-# # ITP1_2_B
-# input = input()
-# a,b,c = [int(num) for num in input.split(' ')]
-# if a < b  and b < c:
-#     print('Yes')
-# else:
-#     print('No')
-#
-# # ITP1_2_C
-# input = input()
-# list = [int(num) for num in input.split(' ')]
-# list.sort()
-# print(f"{list[0]} {list[1]} {list[2]}")
-#
-# #ITP1_2_D
-# # input = input()
-# input = '5 4 2 4 1'
-# W,H,x,y,r = [int(num) for num in input.split(' ')]
-#
-#
-# way = (x + r <= W,y + r <= H,x - r >= 0,y - r >= 0,y - r >= 0)
-# if W >= x and H >= y:
-#     if(all(way)):
-#         print('Yes')
-#     else:
-#         print('No')
-# else:
-#     print('No')
+input = '5 4 2 4 1'
+W,H,x,y,r = [int(num) for num in input.split(' ')]
+
+
+way = (x + r <= W,y + r <= H,x - r >= 0,y - r >= 0,y - r >= 0)
+if W >= x and H >= y:
+    if(all(way)):
+        print('Yes')
+    else:
+        print('No')
+else:
+    print('No')
 
 # ITP1_3_A
 for _ in range(1000):
     print('Hello World')
+
+# ITP1_3_C
+while True:
+    x, y = map(int, input().split())
+    if x == 0 and y == 0:
+        break
+    elif x < y:
+        print(x, y)
+    else:
+        print(y, x)
