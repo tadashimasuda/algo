f63023ad9a71d2e707dd371cdc336da540878157
tadashimasuda <tmasu4260@gmail.com>
2021-10-04 17:14:01 +0900

    bubble update

--- sort/bubble.py
+++ sort/bubble.py
@@ -1,17 +1,27 @@
 from typing import List
 
-def bubble_v1(numbers:List[int])->List[int]:
+# def bubble_v1(numbers:List[int])->List[int]:
+#
+#     for limit in reversed(range(len(numbers))):
+#         j = 0
+#
+#         while j < limit:
+#             if numbers[j] > numbers[j+1]:
+#                 numbers[j],numbers[j+1] = numbers[j+1],numbers[j]
+#             j += 1
+#
+#     return numbers
 
-    for limit in reversed(range(len(numbers))):
-        j = 0
+def bubble_v2(numbers:List[int]) -> List[int]:
+    len_numbers = len(numbers)
 
-        while j < limit:
+    for i in range(len_numbers):
+        for j in range(len_numbers-1-i):
             if numbers[j] > numbers[j+1]:
-                numbers[j],numbers[j+1] = numbers[j+1],numbers[j]
-            j += 1
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers[j]
 
     return numbers
 
 if __name__ == '__main__':
     numbers = [2,5,1,8,7,3]
-    print(bubble(numbers))
+    print(bubble_v2(numbers))
