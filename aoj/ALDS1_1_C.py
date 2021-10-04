c3cdd571614f0eadfc1fb8abd27f762a006085c9
tadashimasuda <tmasu4260@gmail.com>
2021-10-04 16:14:45 +0900

    ALDS1_1_C

---
+++ aoj/ALDS1_1_C.py
@@ -0,0 +1,28 @@
+import math
+def is_prime(number:int)->bool:
+    if number == 2:
+        return True
+    if number<2 and number % 2 == 0:
+        return False
+
+    i=3a
+    while i <= math.sqrt(number):
+        if number % i == 0:
+            return False
+        i = i+2
+
+    return True
+
+if  __name__ == '__main__':
+    judge_num = int(input())
+    numbers=[]
+    count=0
+    while count<judge_num:
+        numbers.append(int(input()))
+        count+=1
+
+    count = 0
+    for i in range(2,len(numbers)):
+        if is_prime(i):
+            count+=1
+    print(count)
