eb97556662038c82b05048dc7ad51747ac9883b7
tadashimasuda <tmasu4260@gmail.com>
2021-10-25 22:40:38 +0900

    add B

--- atcoder/abc224/B.py
+++ atcoder/abc224/B.py
@@ -1,2 +1,30 @@
 #https://atcoder.jp/contests/abc224/tasks/abc224_b
 
+H,W = map(int,input().split(' '))
+A = []
+count = 0
+while True:
+
+    if (count ==H):
+        break
+
+    input_list = [ int(_) for _ in input().split(' ')]
+    A.append(input_list)
+    
+    count+=1
+
+flag = False
+for h in range(H):
+    for w in range(W):
+        if((h < h+1 < H) and (w < w+1 < W)):
+            left = A[h][w] + A[h+1][w+1]
+            right = A[h+1][w] + A[h][w+1]
+            if(not left <= right):
+                flag = True
+                break
+
+if(not flag):
+    print("Yes")
+else:
+    print("No")
+
