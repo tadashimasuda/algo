f84a1562c1421c13452a4da23d3027f8414bed04
tadashimasuda <tmasu4260@gmail.com>
2021-11-06 00:05:58 +0900

    abc068_b finish

--- atcoder/problems/abc068_b.py
+++ atcoder/problems/abc068_b.py
@@ -3,9 +3,9 @@ N = int(input())
 ans = 0
 cnt = 1
 
-for i in range(2,N):
-    if cnt * 2 <= N:
-        ans = i
+for i in range(N):
+    if 2**i <= N:
         cnt *= 2
+        ans = 2**i
 
 print(ans)
