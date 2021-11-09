8d39e1a361426b16196c27e9fbfaffba2f8ec51a
tadashimasuda <tmasu4260@gmail.com>
2021-11-10 00:23:20 +0900

    abc142_c

--- atcoder/problems/abc142_c.py
+++ atcoder/problems/abc142_c.py
@@ -1,2 +1,9 @@
 N = int(input())
-A = map(int,input().split(' '))
+A = list(map(int,input().split(' ')))
+
+ans = [0]*N
+for i,num in enumerate(A,1):
+    ans[num-1] = i
+
+ans_text = " ".join([str(_) for _ in ans])
+print(ans_text)
