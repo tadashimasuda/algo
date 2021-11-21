09ba47448b1f81d0ed41e945ea322cb1a88d2da7
tadashimasuda <tmasu4260@gmail.com>
2021-11-21 23:42:07 +0900

    AC

--- atcoder/problems/agc027_a.py
+++ atcoder/problems/agc027_a.py
@@ -3,18 +3,17 @@ a = list(map(int,input().split(' ')))
 
 a.sort()
 cnt=0
-if a[0]>x:
-    print(0)
-    exit()
-    
-for i in a:
-    if x>=i:
-        x-=i
+
+for i in range(N-1):
+    if x>=a[i]:
+        x-=a[i]
         cnt+=1
     else:
         break
 
-if x>0:
-    cnt-=1
+if x==a[N-1]:
+    cnt+=1
+
 print(cnt)
+
 
