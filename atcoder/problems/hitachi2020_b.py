0d89143f0771abccc13a0be33b7a0da69d37d1ad
tadashimasuda <tmasu4260@gmail.com>
2021-11-26 23:35:38 +0900

    hitachi2020b

--- atcoder/problems/hitachi2020_b.py
+++ atcoder/problems/hitachi2020_b.py
@@ -1,1 +1,10 @@
 A,B,M = map(int,input().split(' '))
+a = list(map(int,input().split(' ')))
+b = list(map(int,input().split(' ')))
+xyc = [list(map(int,input().split(' '))) for _ in range(M)]
+
+min_A,min_B = min(a),min(b)
+ans = min_A+min_B
+for x,y,c in xyc:
+    ans =min(a[x-1]+b[y-1]-c,ans)
+print(ans)
